3cccc1bf67833f07d7c477078de89f9c6623c700
Aaron Parecki <aaron@parecki.com>
2017-05-06 09:14:36 -0700

    reformat list of tests as a table

--- views/subscriber/index.php
+++ views/subscriber/index.php
@@ -8,26 +8,62 @@
     <h2>Testing your Subscriber</h2>
 
     <h3>Discovery</h3>
-    <ul>
-      <li><a href="/subscriber/100">100</a> - HTTP header discovery</li>
-      <li><a href="/subscriber/101">101</a> - HTML tag discovery</li>
-      <li><a href="/subscriber/102">102</a> - Atom feed discovery</li>
-      <li><a href="/subscriber/103">103</a> - RSS feed discovery</li>
-      <li><a href="/subscriber/104">104</a> - Discovery priority</li>
-    </ul>
+    <table class="ui compact table">
+      <tr>
+        <td width="40"></td>
+        <td width="50"><a href="/subscriber/100">100</a></td>
+        <td>HTTP header discovery</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><a href="/subscriber/101">101</a></td>
+        <td>HTML tag discovery</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><a href="/subscriber/102">102</a></td>
+        <td>Atom feed discovery</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><a href="/subscriber/103">103</a></td>
+        <td>RSS feed discovery</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><a href="/subscriber/104">104</a></td>
+        <td>Discovery priority</td>
+      </tr>
+    </table>
 
     <h3>Subscribing</h3>
-    <ul>
-      <li><a href="/subscriber/200">200</a> - Subscribing to a URL that reports a different rel=self</li>
-      <li><a href="/subscriber/201">201</a> - Subscribing to a temporarily redirected topic</li>
-      <li><a href="/subscriber/202">202</a> - Subscribing to a permanently redirected topic</li>
-      <li><a href="/subscriber/203">203</a> - Subscribing to a temporarily redirected hub</li>
-      <li><a href="/subscriber/204">204</a> - Subscribing to a permanently redirected hub</li>
-      <li>more soon...</li>
-      <!-- 
-      <li><a href="/subscriber/103">103</a> - Test unsubscribing</li>
-      -->
-    </ul>
+    <table class="ui compact table">
+      <tr>
+        <td width="40"></td>
+        <td width="50"><a href="/subscriber/200">200</a></td>
+        <td>Subscribing to a URL that reports a different rel=self</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><a href="/subscriber/201">201</a></td>
+        <td>Subscribing to a temporarily redirected topic</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><a href="/subscriber/202">202</a></td>
+        <td>Subscribing to a permanently redirected topic</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><a href="/subscriber/203">203</a></td>
+        <td>Subscribing to a temporarily redirected hub</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><a href="/subscriber/204">204</a></td>
+        <td>Subscribing to a permanently redirected hub</td>
+      </tr>
+    </table>
 
     <!-- 
     <h3>Error Handling</h3>
