20ade012364766f73805e315993c9096cb6a51a7
Aaron Parecki <aaron@parecki.com>
2017-05-19 18:24:12 +0200

    remove old dashboard template, add impl report to home page

--- views/index.php
+++ views/index.php
@@ -45,6 +45,13 @@
     <p>If you are writing a standalone open hub, you can enter the hub URL in the tests and a temporary blog will be created that points to your hub. If you are writing an integrated hub, then you can provide a URL that advertises your hub to test it, in which case you'll need to trigger adding new posts manually.</p>
   </section>
 
+  <section class="content">
+    <h2>Implementation Reports</h2>
+
+    <p>As you're working through the websub.rocks tests, please fill out an implementation report to update the W3C working group about your implementation!</p>
+    <p>It isn't too hard to do. Just fork the WebSub repo, and copy the appropriate <a href="https://github.com/w3c/websub/tree/master/implementation-reports">implementation report template</a> to a new file, check off the boxes that apply to you, then submit a pull request.</p>
+  </section>
+
   <section class="content small">
     <p>This code is <a href="https://github.com/aaronpk/websub.rocks">open source</a>. Feel free to <a href="https://github.com/aaronpk/websub.rocks/issues">file an issue</a> if you notice any errors.</p>
   </section>
